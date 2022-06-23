f32c05e2ba6f1c91ba774a708a00084ec0a22ce1
siguiqiang <siguiqiang@odianyun.com>
2022-06-23 22:05:20 +0800

    基本完成

--- src/main/java/cn/lvhaosir/paramater/CarsRepairParameter.java
+++ src/main/java/cn/lvhaosir/paramater/CarsRepairParameter.java
@@ -2,6 +2,7 @@ package cn.lvhaosir.paramater;
 
 import cn.lvhaosir.entity.CarsRepair;
 import cn.lvhaosir.utils.PageParam;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -15,10 +16,13 @@ public class CarsRepairParameter extends PageParam {
     private String carNumber;
     private String carsRepairType;
     private String carsRepairText;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date endTime;
     private String status;
     private Integer isDelete;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
     private String userName;
     private String phone;
